1fa2d588e455b496a35bc58b8928f9859effe725
sheatipton <set58568@uga.edu>
2023-01-30 15:01:09 -0500

    Initial Commit

---
+++ Capstone/CapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneApp.swift
+//  Capstone
+//
+//  Created by Shea Tipton on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
